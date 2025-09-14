9c38c9a3772f97e7d0d44e3e9dd76db5e6a5beaf
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-09-14 15:24:37 +0530

    readung csv rows

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@ use std::error::Error;
 use csv;
 
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(path)?;
+    //   Result<T, E> is Rust’s way of saying:
+    // → Success: return T
+    // → Error: return E
+    // Here success is () (unit type = “nothing to return”).
+    // Errors are wrapped in Box<dyn Error> → “any kind of error in a heap-allocated box.”
+
+    let mut reader = csv::Reader::from_path(path)?; // `?` is Rust’s error propagation operator: If it succeeds → return the reader. If it fails → immediately return the error (Err) from this function.
 
     for result in reader.records(){
         let record = result?;
 
-        println!("{:?}", record)
+        println!("{:?}", record) //{:?} is the debug format (shows the record in a human-readable way).
     }
-    Ok(())
+    Ok(()) //After reading all rows successfully, return Ok(()) → success, no errors.
 }
 
 fn main() {
